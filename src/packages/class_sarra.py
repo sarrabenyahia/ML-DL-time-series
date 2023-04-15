300d6eac42cb7dec39c79bb3491f70a7f896a83d
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-04-15 23:42:37 +0200

    ok

--- src/packages/class_sarra.py
+++ src/packages/class_sarra.py
@@ -5,20 +5,35 @@ import numpy as np
 from statsmodels.graphics.gofplots import ProbPlot
 
 class DataPlotter:
-    def __init__(self, df, column):
+    def __init__(self, df ):
         self.df = df
-        self.column = column
 
-    def plot(self):
+    def plotTS(self, column):
         """
         Plots the given column from the given DataFrame
         """
         plt.figure(figsize=(15,5))
-        ax = sns.lineplot(data=self.df[self.column])
-        ax.set(title=f'{self.column} plot for {self.df.index.name}', xlabel='Date' if 'date' in self.df.index.name.lower() else 'Week', ylabel=self.column)
+        ax = sns.lineplot(data=self.df[column])
+        ax.set(title=f'{column} plot for {self.df.index.name}', xlabel='Date' if 'date' in self.df.index.name.lower() else 'Week', ylabel=column)
 
-    def plot_prob(self):
+
+    def plot_prob(self, column) :
         """
         Plots the probability plot for the given column from the given DataFrame
         """
-        qq = ProbPlot(self.df[self.column]).qqplot(line='s')
+        qq = ProbPlot(self.df[column]).qqplot(line='s')
+
+
+    def plot_histogram(self, column):
+        """
+        Plots a histogram of the given column from the given DataFrame
+        """
+        plt.figure(figsize=(15,5))
+        ax = sns.histplot(data=self.df[column], bins=50, kde=True, stat="density", alpha=0.6)
+        mu, sigma = stats.norm.fit(self.df[column])
+        xmin, xmax = plt.xlim()
+        x = np.linspace(xmin, xmax, 100)
+        p = stats.norm.pdf(x, mu, sigma)
+        sns.lineplot(x=x, y=p, color='black')
+        ax.set(title=f'Distribution of {column}', xlabel=column, ylabel='Density')
+        
