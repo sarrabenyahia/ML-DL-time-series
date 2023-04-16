d46eb486b0e3b1a34fc5f0c8b91653aa6bd20d12
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-04-16 21:04:10 +0200

    dataprocessor

--- src/packages/Processing.py
+++ src/packages/Processing.py
@@ -5,9 +5,24 @@ from scipy.fft import fft
 from datetime import datetime
 
 
-class FeatureEngineer:
+class DataProcessor:
     def __init__(self, df):
         self.df = df
+
+    def process_data(self, numeric_cols):
+        self.__combine_datetime()
+        self.__convert_numeric(numeric_cols)
+        self.__convert_date()
+        self.__convert_time()
+        self.__extract_date_components()
+        self.__downcast()
+        # ramener la cible à la bonne unité, nécéssaire avant la réalisation de l'EDA et les analyses descriptives
+        self.df['Global_active_power_Wh'] = self.df['Global_active_power']*1000/60
+        # création du other_submetering
+        self.df['other_submetering'] = self.df['Global_active_power_Wh'] - \
+            self.df['Sub_metering_1'] - \
+            self.df['Sub_metering_2'] - self.df['Sub_metering_3']
+        return self.df
 
     def __combine_datetime(self):
         # Combiner les colonnes 'Date' et 'Time' en une seule colonne datetime
@@ -75,81 +90,3 @@ class FeatureEngineer:
                 else:
                     self.df[cols[i]] = self.df[cols[i]].astype('category')
         return self.df
-
-    def process_data(self,numeric_cols):
-        self.__combine_datetime()
-        self.__convert_numeric(numeric_cols)
-        self.__convert_date()
-        self.__convert_time()
-        self.__extract_date_components()
-        self.__downcast()
-        # ramener la cible à la bonne unité, nécéssaire avant la réalisation de l'EDA et les analyses descriptives
-        self.df['Global_active_power_Wh'] = self.df['Global_active_power']*1000/60
-        # création du other_submetering
-        self.df['other_submetering'] = self.df['Global_active_power_Wh'] - \
-            self.df['Sub_metering_1'] - \
-            self.df['Sub_metering_2'] - self.df['Sub_metering_3']
-        return self.df
-
-    def lag_creation(self, lag, time="day"):
-        if isinstance(lag, int):
-            lag = [lag]
-        if time == "day":
-            lag_min = [l*60*24 for l in lag]
-        elif time == "hour":
-            lag_min = [l*60 for l in lag]
-        elif time == "minute":
-            lag_min = lag
-        else:
-            raise ValueError(
-                "Invalid time value. Please use 'day', 'hour' or 'minute.")
-
-        new_cols = []
-        for col in self.df.columns:
-            for index, l in enumerate(lag_min):
-                new_col = self.df[col].shift(l)
-                new_col.name = f"{col}_lag{lag[index]}{time[0]}"
-                new_cols.append(new_col)
-
-        return pd.concat([self.df] + new_cols, axis=1)
-
-    def window_rolling(self, window_size, time="day"):
-        if isinstance(window_size, int):
-            window_size = [window_size]
-            if time == "day":
-                window_min = [l*60*24 for l in window_size]
-            elif time == "hour":
-                window_min = [l*60 for l in window_size]
-            elif time == "minute":
-                window_min = window_size
-            else:
-                raise ValueError(
-                    "Invalid time value. Please use 'day', 'hour' or 'minute.'")
-
-        for window in window_min:
-            self.df[f'Global_active_power_mean_{window}'] = self.df['Global_active_power'].rolling(
-                window).mean()
-            self.df[f'Global_active_power_min_{window}'] = self.df['Global_active_power'].rolling(
-                window).min()
-            self.df[f'Global_active_power_max_{window}'] = self.df['Global_active_power'].rolling(
-                window).max()
-            self.df[f'Global_active_power_std_{window}'] = self.df['Global_active_power'].rolling(
-                window).std()
-
-    def create_time_based_features(self):
-        self.df['hour'] = self.df.index.hour
-        self.df['is_daytime'] = np.where(
-            self.df['hour'].isin(range(7, 20)), 1, 0)
-        self.df['is_night_time'] = np.where(self.df['hour'].isin(
-            range(0, 7)) | self.df['hour'].isin(range(20, 24)), 1, 0)
-
-    def create_fourier_transform_features(self, num_features=5):
-        fft_vals = fft(self.df['Global_active_power'])
-        abs_fft_vals = np.abs(fft_vals)
-        num_samples = len(self.df)
-        freqs = np.arange(num_samples) / num_samples
-        top_freqs = freqs[np.argsort(-abs_fft_vals)[:num_features]]
-
-        for i, freq in enumerate(top_freqs, 1):
-            self.df[f'Global_active_power_freq_{i}'] = abs_fft_vals[int(
-                freq*num_samples)]
